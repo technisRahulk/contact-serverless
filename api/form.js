2f97feb5f1bd4cc6528e969e935fde31822326b4
technisRahulk <rahulkumaru99@gmail.com>
2021-02-10 19:19:47 +0530

    initial commit

---
+++ api/form.js
@@ -0,0 +1,43 @@
+const nodemailer=require('nodemailer');
+require("dotenv").config();
+
+module.exports= (req,res) =>{
+     if(req.method=="POST"){
+        let data =req.body;
+        //transporter object
+        const transporter = nodemailer.createTransport({
+            service: 'Gmail',
+            //credentials of the sender
+            auth: {
+                user: process.env.EMAIL,
+                pass: process.env.PASS,
+            },
+        });
+        // verify connection configuration
+        transporter.verify(function (error, success) {
+            if (error) {
+            console.log(error);
+            } else {
+            console.log("Server is ready to take our messages");
+            }
+        });
+        const mail = {
+            from: data.name,
+            //email of reciever 
+            to:process.env.RECP_EMAIL,
+            subject: data.subject,
+            text: `${data.name} <${data.email}> \n${data.message}`,
+        };
+        transporter.sendMail(mail, (err, data) => {
+            if (err) {
+            console.log(err);
+            res.status(500).send("Something went wrong.");
+            } else {
+            res.status(200).send("Email successfully sent to recipient!");
+            }
+        });
+     }
+     else{
+         res.send("Welcome to serverless contact form")
+     }
+}
